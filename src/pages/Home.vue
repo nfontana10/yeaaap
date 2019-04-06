ea5460f6d6c3f238b8ba986772c6d1df1175141d
Nick Fontana <nfontana10@gmail.com>
2019-04-06 18:18:38 +0200

    totallygoingsofast

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <h1 class="title">YEAAAAAAAAP!</h1>
-    <img class="main_image" alt="zati" src="../assets/zati.jpg"/>
+    <div class="crazy_images">
+      <img class="main_image" alt="zati" src="../assets/zati.jpg"/>
+      <img class="nick" alt="zati" src="../assets/nick.jpg"/>
+      <img class="levi" alt="zati" src="../assets/levi.jpg"/>
+      <img class="henry" alt="zati" src="../assets/henry.jpg"/>
+    </div>
+
     
   </div>
 </template>
@@ -34,8 +40,16 @@ a {
 .main_image {
   max-width: 100%;
   width: 500px;
+  border: 2px solid black;
 }
 .title {
   border-bottom: 1px solid #000000;
+  font-size: 36px;
+}
+.nick,
+.levi,
+.henry {
+  border: 3px solid #000000;
+  width: 100px;
 }
 </style>
