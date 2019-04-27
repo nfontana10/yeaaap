8d6f826d9fde0ca02d54ad1112759f01843e310b
levimykel <levimykel@gmail.com>
2019-04-27 16:42:12 +0200

    Switch to SCSS

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,15 +5,15 @@
       <div class="left">
         <h2 class="meet_our_founders">Meet Our Founders...</h2>
         <div class="menu_left">
-          <img class="nick" alt="nick" src="../assets/nick.jpg"/>
+          <img alt="nick" src="../assets/nick.jpg"/>
           <p class="quote">Only now is the child finally divested of all that he has been. His origins are become remote as is his destiny and not again in all the world's turning will there be terrains so wild and barbarous to try whether the stuff of creation may be shaped to man's will or whether his own heart is not another kind of clay.</p>
         </div>
         <div class="menu_right">
-          <img class="levi" alt="levi" src="../assets/levi.jpg"/>
+          <img alt="levi" src="../assets/levi.jpg"/>
           <p class="quote">I'd rather laugh with the sinners than cry with the saints, the sinners have much more fun.</p>
         </div>
         <div class="menu_left">
-          <img class="henry" alt="henry" src="../assets/henry.jpg"/>
+          <img alt="henry" src="../assets/henry.jpg"/>
           <p class="quote">hangin' round</p>
         </div>
       </div>
@@ -53,19 +53,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.crazy_images .left {
-  width: 40%;
-  float: left;
-  margin-left: 50px;
-}
-.crazy_images .left img:hover{
-  /* Start the shake animation and make the animation last for 0.5 seconds */
-  animation: shake 0.5s; 
-
-  /* When the animation is finished, start again */
-  animation-iteration-count: infinite; 
-}
+<style lang="scss" scoped>
 @keyframes shake {
   0% { transform: translate(1px, 1px) rotate(0deg); }
   10% { transform: translate(-1px, -2px) rotate(-1deg); }
@@ -79,39 +67,62 @@ export default {
   90% { transform: translate(1px, 2px) rotate(0deg); }
   100% { transform: translate(1px, -2px) rotate(-1deg); }
 }
-.main_image {
-  max-width: 100%;
-  width: 500px;
-  border: 2px solid black;
-}
+
 .title {
   border-bottom: 1px solid #000000;
   font-size: 36px;
 }
-.menu_left,
-.menu_right {
-  min-height: 200px;
-  margin-bottom: 20px;
+
+.crazy_images {
+  .left {
+    width: 40%;
+    float: left;
+    margin-left: 50px;
+
+    img {
+      border: 2px solid #000000;
+      width: 100px;
+      filter: gray;
+      -webkit-filter: grayscale(1);
+      filter: grayscale(1);
+      
+      &:hover{
+        /* Start the shake animation and make the animation last for 0.5 seconds */
+        animation: shake 0.5s;
+      }
+    }
+
+    .menu_left {
+      min-height: 200px;
+      margin-bottom: 20px;
+
+      img {
+        float: left;
+      }
+
+      .quote {
+        margin-left: 120px;
+      }
+    }
+
+    .menu_right {
+      min-height: 200px;
+      margin-bottom: 20px;
+
+      img {
+        float: right;
+      }
+
+      .quote {
+        margin-left: 120px;
+      }
+    }
+  }
 }
-.menu_left img {
-  float: left;
-}
-.menu_right img {
-  float: right;
-}
-.menu_left .quote {
-  margin-left: 120px;
-}
-.menu_right .quote {
-  margin-left: 120px;
-}
-.nick,
-.levi,
-.henry {
-  border: 2px solid #000000;
-  width: 100px;
-  filter: gray;
-  -webkit-filter: grayscale(1);
-  filter: grayscale(1);
+
+.main_image {
+  max-width: 100%;
+  width: 500px;
+  border: 2px solid black;
 }
 </style>
